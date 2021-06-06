ccb442ff919ee4a27a3b1227d9ec273168cc7551
rashu11 <69381120+rashu11@users.noreply.github.com>
2021-06-07 00:17:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'daisy' in command:
+                command = command.replace('daisy', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_daisy():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'hello' in command:
+        talk('hello')
+    elif 'are you free' in command:
+        talk('sorry, I am busy')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'introduce yourself' in command:
+        talk('Hey, I am your daisy. What can I do for you')
+    elif 'happy' in command:
+        talk('same here')
+    else:
+        talk('Please say the command again')
+
+
+while True:
+    run_daisy()
